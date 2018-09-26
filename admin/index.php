8c74bca3c2d9f85a52a3c76520e38b237cd47f5c
heathu <876734392@qq.com>
2018-09-26 18:18:40 +0800

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -4,8 +4,8 @@ define("SP_PATH",dirname(__FILE__).'/SpeedPHP');
 $spConfig = array(
 	"db" => array( // 数据库设置
 		'host' => 'localhost',  // 数据库地址，一般都可以是localhost
-		'login' => 'root', // 数据库用户名
-		'password' => '', // 数据库密码
+		'login' => 'library', // 数据库用户名
+		'password' => 'library', // 数据库密码
 		'database' => 'lib', // 数据库的库名称
 	),
 	'view' => array(
